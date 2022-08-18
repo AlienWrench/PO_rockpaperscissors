5d757aa8d0701ef7439b93021381c3c4c5caae53
Cody Allen <AlienWrench46@gmail.com>
2022-08-17 19:52:54 -0600

    add function getPlayerChoice for players to choose a hand to play, along with functions to ensure that choice is valid.

--- rochambeau.js
+++ rochambeau.js
@@ -10,4 +10,36 @@ function getComputerChoice() {
     const computerChoice = possibleChoices[arraySelector];
 
     return computerChoice;
+};
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Please choose a hand to play (Rock, Paper, Scissors): ");
+
+    return playerChoice;
+};
+
+//function for case insensitivity? could call from getPlayerChoice?
+function makeChoiceInsensitive(playerChoice) {
+    const insensitiveChoice = playerChoice.toLowerCase();
+
+    return insensitiveChoice;
 }
+
+//function for checking validity of player response? if not valid, prompt again. could call from getPlayerChoice?
+function isChoiceValid(playerChoice) {
+    const validChoice1 = "rock";
+    const validChoice2 = "paper";
+    const validChoice3 = "scissors";
+
+    playerChoice = makeChoiceInsensitive(playerChoice);
+
+    if (playerChoice === validChoice1 || playerChoice === validChoice2 || playerChoice === validChoice3) {
+        return playerChoice;
+    } else {
+        console.log("Sorry. That is not a valid choice. Please try again.");
+        getPlayerChoice();
+    }
+}
+
+let globalPlayerChoice = isChoiceValid(getPlayerChoice());  //or I guess call it like this
+//function for actually playing the game
